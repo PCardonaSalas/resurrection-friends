6bbe5ebd362f538b6fc5e1b06bb81c168d3f0ae9
Pablo <pcardonasalas@gmail.com>
2025-06-19 16:14:47 +0200

    fix: corrige configuración de despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    assets: "_assets"
-  },
-  base: '/resurrection-friends',
   output: 'static',
-  site: 'https://pcardonasalas.github.io',
-  trailingSlash: 'always' // Esto ayuda con la compatibilidad de GitHub Pages
+  base: '/resurrection-friends',
+  site: 'https://pcardonasalas.github.io'
 });
